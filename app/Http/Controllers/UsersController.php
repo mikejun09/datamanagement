75cd930d0181027214c584b65a2d8a8a0cdf3db2
mikejun09 <mikejunzaballero09@gmail.com>
2025-03-01 02:06:09 +0800

    111

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -159,29 +159,31 @@ public function household_leader(Request $request)
 
 public function search_voter(Request $request)
 {
-
     $barangays = Barangay::all();
-    
-
 
     $barangay = $request->input('barangay');
     $first_name = $request->input('first_name');
     $last_name = $request->input('last_name');
 
-    // Default: Select all voters if no search query is provided
-    $voters = MasterList::when($barangay, function ($query, $barangay) {
-            return $query->where('barangay', 'like', "%{$barangay}%");
-        })
-        ->when($first_name, function ($query, $first_name) {
-            return $query->where('first_name', 'like', "%{$first_name}%");
-        })
-        ->when($last_name, function ($query, $last_name) {
-            return $query->where('last_name', 'like', "%{$last_name}%");
-        })
-        ->get();
+    // Only fetch voters if at least one search parameter is provided
+    $voters = collect(); // Empty collection by default
+    if ($barangay || $first_name || $last_name) {
+        $voters = MasterList::when($barangay, function ($query, $barangay) {
+                return $query->where('barangay', 'like', "%{$barangay}%");
+            })
+            ->when($first_name, function ($query, $first_name) {
+                return $query->where('first_name', 'like', "%{$first_name}%");
+            })
+            ->when($last_name, function ($query, $last_name) {
+                return $query->where('last_name', 'like', "%{$last_name}%");
+            })
+            ->get();
+    }
 
-    return view('admin.search', compact('voters' , 'barangays'));
+    return view('user.user_search', compact('voters', 'barangays'));
 }
+
+
 
 
 }
